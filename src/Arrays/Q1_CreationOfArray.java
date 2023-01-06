2b28b3913351ed8cebb205cbd46ef4af8e833dca
Muhammad Shayan <21sw067@students.muet.edu.pk>
2023-01-06 12:23:16 -0800

    Input and Output of Array

--- src/Arrays/Q1_CreationOfArray.java
+++ src/Arrays/Q1_CreationOfArray.java
@@ -5,11 +5,11 @@ public class Q1_CreationOfArray {
     public static void main(String[] args) {
 
 
-        int [] marks = new int[50];
+        int[] marks = new int[50];
 
-        int [] numbers = {1,3,4,5,8,6};
+        int[] numbers = {1, 3, 4, 5, 8, 6};
 
-        String [] fruits = {"Mango" , "Apple" , "Banana"};
+        String[] fruits = {"Mango", "Apple", "Banana"};
 
     }
 
