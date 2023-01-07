060bce696d91042d11c7a0027e1af24dfe3e137e
Muhammad Shayan <21sw067@students.muet.edu.pk>
2023-01-07 12:53:32 -0800

    Creation of strings

---
+++ src/Strings/Strings.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class Strings {
+
+    //Creation of strings
+    //Strings are IMMUTABLE
+
+    char[] str= {'a','b','c'};
+    String str1 = "abcd";
+    String str2 = new String("abcd");
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
