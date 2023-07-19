c4d62f4b4eb9534012f09f14477fa5e884d7985c
Muhammad Shayan <21sw067@students.muet.edu.pk>
2023-07-19 20:05:20 +0100

    rotation of array

--- src/Arrays/TwoPointers.java
+++ src/Arrays/TwoPointers.java
@@ -16,9 +16,7 @@ public class TwoPointers{
 
     }
 
-    public static void reverse(int [] arr){
-
-        int i = 0 , j = arr.length-1;
+    public static void reverse(int [] arr, int i , int j){
 
         while(i < j){
 
@@ -29,6 +27,18 @@ public class TwoPointers{
         }
     }
 
+    public static void rotateInPlace(int [] arr , int k){
+
+        int n = arr.length;
+        k = k % n ;
+
+        reverse(arr , 0 , n-k-1);
+        reverse(arr, n-k, n-1);
+        reverse(arr, 0, n-1);
+
+
+
+    }
 
 
 
@@ -36,8 +46,13 @@ public class TwoPointers{
 
         int[] arr1 = { 5,4,3,2,1};
 
-        reverse(arr1);
+        reverse(arr1, 0 , arr1.length-1);
         System.out.println(Arrays.toString(arr1));
+
+        int [] arr2 = {1,2,3,4,5,6,7};
+
+        rotateInPlace(arr2, 8);
+        System.out.println(Arrays.toString(arr2));
 
 
 
